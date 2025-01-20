48aede7d7ceda8d9d696d2e3d9d3f8686fee5904
Anantaraj Katti <anantarajkatti@gmail.com>
2025-01-20 18:23:53 +0530

    third commit

---
+++ Dom.js
@@ -0,0 +1,5 @@
+let parentDiv= document.getElementById("parentDiv")
+let childDiv=document.createElement('div')
+childDiv.innerHTML="hello Brother"
+
+parentDiv.appendChild(childDiv)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,10 +1,49 @@
-function hello(callback){
-//     let name='Anantaraj'
-//     callback(name)
-// }
+function getUserData(userId, callback) {
+    setTimeout(() => {
+        console.log(`Fetched data for user ${userId}`);
+        callback(userId);                                         //(userId) => {
+                                                                         //     getPosts(userId, (postId) => {
+                                                                        //                                     getComments(postId,   () => {
+                                                                        //                                                                  console.log("Fetched all data");
+                                                                        //                                                                  } 
+                                                                        //                                                 ); 
+                                                                                 //                          }
+                                                                                 //     );
+                                                                             // }   
+    }, 1000);
+}
 
-// function surname(name){
-//     console.log(`${name} Katti`)
-// }
+function getPosts(userId, callback) {
+    setTimeout(() => {
+        console.log(`Fetched posts for user ${userId}`);
+        callback(userId);
+                                                                    //(postId) => {
+                                                                        //         getComments(postId,   () => {
+                                                                        //                                     console.log("Fetched all data");
+                                                                        //                                      } 
+                                                                        //
 
-// hello(surname)
+    }, 1000);
+}
+
+function getComments(postId, callback) {
+    setTimeout(() => {
+        console.log(`Fetched comments for post ${postId}`);
+        callback();         
+                                                                                 //() => {
+                                                                        //             console.log("Fetched all data");
+                                                                        //         }
+
+    }, 1000);
+}
+
+
+getUserData(1, (userId) => {
+                            getPosts(userId, (postId) => {
+                                                            getComments(postId, () => {
+                                                                                        console.log("Fetched all data");
+                                                                                        }
+                                                                         );
+                                                            }
+                                    );
+                            }
